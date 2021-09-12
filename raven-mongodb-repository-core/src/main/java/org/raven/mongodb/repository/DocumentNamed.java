30a37238cf0c4b07e332cca47bfdc5985034fe5c
yi.liang <indifer@126.com>
2021-09-12 22:46:01 +0800

    DocumentNamed supper Contract#value

--- raven-mongodb-repository-core/src/main/java/org/raven/mongodb/repository/DocumentNamed.java
+++ raven-mongodb-repository-core/src/main/java/org/raven/mongodb/repository/DocumentNamed.java
@@ -3,6 +3,7 @@ package org.raven.mongodb.repository;
 import org.raven.commons.data.MemberFormatType;
 import org.raven.commons.data.MemberFormatUtils;
 import org.raven.commons.data.annotation.Contract;
+import org.raven.commons.util.StringUtils;
 
 import java.lang.annotation.Annotation;
 
@@ -15,14 +16,16 @@ public abstract class DocumentNamed {
     public static String getNamed(Class<?> documentClass) {
 
         MemberFormatType formatType = MemberFormatType.CamelCase;
+        String name = null;
         for (final Annotation annotation : documentClass.getAnnotations()) {
             if (annotation instanceof Contract) {
                 formatType = ((Contract) annotation).formatType();
+                name = ((Contract) annotation).value();
                 break;
             }
         }
 
-        String name = documentClass.getSimpleName();
+        name = StringUtils.isBlank(name) ? documentClass.getSimpleName() : name;
         name = MemberFormatUtils.namingFormat(name, formatType);
         return name;
     }
