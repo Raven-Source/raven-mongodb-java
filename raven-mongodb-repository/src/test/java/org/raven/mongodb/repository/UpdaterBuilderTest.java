e16ff687cf94875c330bb82305df7e914135634e
yi.liang <indifer@126.com>
2021-09-13 21:59:06 +0800

    add UpdateBuilder add FilterBuilder

--- raven-mongodb-repository/src/test/java/org/raven/mongodb/repository/UpdaterBuilderTest.java
+++ raven-mongodb-repository/src/test/java/org/raven/mongodb/repository/UpdaterBuilderTest.java
@@ -25,6 +25,7 @@ public class UpdaterBuilderTest {
 
         UpdateBuilder<User> updateBuilder = new UpdateBuilder<>(User.class);
         updateBuilder
+                .set("a", 1)
                 .set("b", 2)
                 .unset("c")
                 .setOnInsert("d", 4)
@@ -50,7 +51,7 @@ public class UpdaterBuilderTest {
         log.info(bson.toBsonDocument().toJson());
 
 
-        Assert.assertEquals("{\"$set\": {\"b\": 2}, \"$unset\": {\"c\": \"\"}, \"$setOnInsert\": {\"d\": 4}, \"$rename\": {\"e\": \"ee\"}, \"$inc\": {\"f\": 6}, \"$mul\": {\"g\": 7}, \"$min\": {\"h\": 10}, \"$max\": {\"h\": 11}, \"$currentDate\": {\"j\": true, \"k\": {\"$type\": \"timestamp\"}}, \"$addToSet\": {\"l\": 12, \"m\": {\"$each\": [13, 14, 15]}}, \"$push\": {\"n\": 16, \"o\": {\"$each\": [17, 18, 19]}}, \"$pull\": {\"p\": 20}, \"$pullAll\": {\"q\": [21, 22]}, \"$pop\": {\"r\": -1, \"s\": 1}, \"$bit\": {\"t\": {\"and\": 10}}}"
+        Assert.assertEquals("{\"$set\": {\"a\": 1, \"b\": 2}, \"$unset\": {\"c\": \"\"}, \"$setOnInsert\": {\"d\": 4}, \"$rename\": {\"e\": \"ee\"}, \"$inc\": {\"f\": 6}, \"$mul\": {\"g\": 7}, \"$min\": {\"h\": 10}, \"$max\": {\"h\": 11}, \"$currentDate\": {\"j\": true, \"k\": {\"$type\": \"timestamp\"}}, \"$addToSet\": {\"l\": 12, \"m\": {\"$each\": [13, 14, 15]}}, \"$push\": {\"n\": 16, \"o\": {\"$each\": [17, 18, 19]}}, \"$pull\": {\"p\": 20}, \"$pullAll\": {\"q\": [21, 22]}, \"$pop\": {\"r\": -1, \"s\": 1}, \"$bit\": {\"t\": {\"and\": 10}}}"
                 , bson.toBsonDocument().toJson()
         );
     }
